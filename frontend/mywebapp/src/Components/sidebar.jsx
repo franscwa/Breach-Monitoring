afa96c959f1e1af7a2bc6e452a671b77bda47c84
franscwa <francisco@1c9180cb4cd9.p.fiu.edu>
2022-05-22 16:27:13 -0400

    cleaned up

--- frontend/mywebapp/src/Components/sidebar.jsx
+++ frontend/mywebapp/src/Components/sidebar.jsx
@@ -1,6 +1,21 @@
 import './sidebar.css'
-
+import axios from 'axios';
 function Sidebar() {
+    const [checkEmailBreaches, setEmailBreaches] = useState('');
+
+    const update = () => {
+        axios
+            .get(haveibeenpwned.com/api/v3/dataclasses)
+            .then((res) => {
+                setRandomQuote(res.data);
+            });
+    };
+
+    useEffect(update, []);
+
+   
+
+
     return (
         <>
 <button className='button-class'>New Credential</button>
